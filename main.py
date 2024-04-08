18a6fe886424fae5ed3ab332dff09aeb2db990fc
WebsitesGiveCookies <madalinapopa2008@gmail.com>
2024-04-08 17:57:05 +0300

    uploading

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from cryptography.fernet import Fernet
 from dotenv import load_dotenv
+from os import getenv
+
 # genereate key
-key = Fernet.generate_key()
-print(key)
-load_dotenv
+# key = Fernet.generate_key()
+load_dotenv()
+key = getenv("KEY")
+encryptor = Fernet(key=key.encode())
+message = "message"
+message = encryptor.encrypt(message.encode())
+print(message)
